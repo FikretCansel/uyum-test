2fbb90b25e34f63472dfa3f349cb1984af1deac2
FikretCansel <fikretcansel25@hotmail.com>
2022-10-29 15:49:50 +0300

    fix commit4

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
-import express, { NextFunction, Request, Response, Express } from "express";
-import http from "http";
-import cors from "cors"
-import path from "path";
-import fs from "fs";
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
 
-var bodyParser = require('body-parser');
-
-const app: Express = express();
-const server = http.createServer(app);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
@@ -18,21 +14,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
-  credentials: true
-}));
 
 
-app.get('/', function(req:Request,res:Response,next:NextFunction) {
+app.get('/', function(req,res,next) {
   let result = req.query.result || false;
   res.render('rapport', { result: result });
 });
 
-app.post('/see',(req:Request,res:Response,next:NextFunction)=> {
-  let girlName : string= req.body.girl.toLowerCase();
-  let boyName : string = req.body.boy.toLowerCase();
+app.post('/see',(req,res,next)=> {
+  let girlName = req.body.girl.toLowerCase();
+  let boyName  = req.body.boy.toLowerCase();
 
 
   const newResult= Math.floor(Math.random() * 101);
@@ -65,4 +56,4 @@ app.post('/see',(req:Request,res:Response,next:NextFunction)=> {
 
 
 const port = 3000;
-server.listen(port, () => console.log(`Run on port ${port}`));
+app.listen(port, () => console.log(`Run on port ${port}`));
